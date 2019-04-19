26b9865031bf79b592d7dcb7abf3a782cf153279
Kyryljan Serhij <kyryljan.serhij@gmail.com>
2019-04-19 14:14:38 +0300

    Move overlays to vendor [1/3]

---
+++ config.mk
@@ -0,0 +1,78 @@
+PRODUCT_PACKAGES += \
+    MDroidSystemBlackTheme \
+    MDroidSystemDarkTheme \
+    MDroidSystemUIBlackTheme \
+    MDroidSystemUIDarkTheme \
+    MDroidNotificationBlackTheme \
+    MDroidNotificationDarkTheme \
+    MDroidSettingsBlackTheme \
+    MDroidSettingsDarkTheme \
+    MDroidDialerBlackTheme \
+    MDroidDialerDarkTheme \
+    MDroidEbayBlackTheme \
+    MDroidEbayDarkTheme \
+    MDroidContactsBlackTheme \
+    MDroidContactsDarkTheme \
+    MDroidFilesBlackTheme \
+    MDroidFilesDarkTheme \
+    MDroidMessagingBlackTheme \
+    MDroidMessagingDarkTheme \
+    MDroidPhoneBlackTheme \
+    MDroidPhoneDarkTheme \
+    MDroidTelecomBlackTheme \
+    MDroidTelecomDarkTheme \
+    MDroidCalculatorBlackTheme \
+    MDroidCalculatorDarkTheme \
+    MDroidGboardBlackTheme \
+    MDroidGboardDarkTheme \
+    MDroidGboardLightTheme \
+    MDroidCalendarBlackTheme \
+    MDroidCalendarDarkTheme \
+    MDroidGooglePackageInstallerBlackTheme \
+    MDroidGooglePackageInstallerDarkTheme \
+    MDroidEmailBlackTheme \
+    MDroidEmailDarkTheme \
+    MDroidElevenBlackTheme \
+    MDroidElevenDarkTheme \
+    MDroidRecorderBlackTheme \
+    MDroidRecorderDarkTheme \
+    MDroidXiaomiPartsBlackTheme \
+    MDroidXiaomiPartsDarkTheme \
+    MDroidBlackAccent \
+    MDroidBlueAccent \
+    MDroidBlueLightAccent \
+    MDroidBrownAccent \
+    MDroidCyanAccent \
+    MDroidGreenAccent \
+    MDroidGreenLightAccent \
+    MDroidLimeAccent \
+    MDroidGreyAccent \
+    MDroidGreyBlueAccent \
+    MDroidAmberAccent \
+    MDroidOrangeAccent \
+    MDroidOrangeDeepAccent \
+    MDroidPinkAccent \
+    MDroidPixelAccent \
+    MDroidNetflixAccent \
+    MDroidPurpleAccent \
+    MDroidPurpleDeepAccent \
+    MDroidIndigoAccent \
+    MDroidRedAccent \
+    MDroidTealAccent \
+    MDroidWhiteAccent \
+    MDroidYellowAccent \
+    QStileCircle \
+    QStileCircleTrim \
+    QStileDefault \
+    QStileDualToneCircle \
+    QStileSquircleTrim
+
+ifeq ($(WITH_QCOM_FM),true)
+PRODUCT_PACKAGES += \
+    MDroidFM2BlackTheme \
+    MDroidFM2DarkTheme
+else
+PRODUCT_PACKAGES += \
+    MDroidFMRadioBlackTheme \
+    MDroidFMRadioDarkTheme
+endif
