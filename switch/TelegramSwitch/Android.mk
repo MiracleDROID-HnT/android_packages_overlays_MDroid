Viper-Project/viper_vendor_themes

themes: Switch telegram style [3/3]

themes/switch/Telegram/Android.mk
@@ -1,0 +1,10 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+CAL_MODULE_TAGS := optional
+
+LOCAL_PACKAGE_NAME := SwitchTelegram
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+
+include $(BUILD_PACKAGE)

MiracleDROID-HnT/android_packages_overlays_MDroid

switch/TelegramSwitch/Android.mk
@@ -1,0 +1,10 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+CAL_MODULE_TAGS := optional
+
+LOCAL_PACKAGE_NAME := TelegramSwitch
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+
+include $(BUILD_PACKAGE)